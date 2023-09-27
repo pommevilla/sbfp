a9e90f129bcccc11021f60ccd40dc90a34d80842
Paul Villanueva <pvillanueva13@gmail.com>
2023-09-27 12:07:56 -0500

    Add kmer_counting

---
+++ workflow/rules/feature_engineering.smk
@@ -0,0 +1,36 @@
+#!/bin/bash
+# ---------------------------
+# Rules about feature extraction/engineering
+# Author: Paul Villanueva (github.com/pommevilla)
+# ---------------------------
+
+rule count_kmers:
+    input:
+        ".fastqs_dumped",
+        sras_list = "data/sras.txt",
+        kmer_count_script = "workflow/scripts/feature_engineering/get_kmer_counts.sh"
+    output:
+        touch(".kmers_counted")
+    log:
+        err = "logs/count_kmers.err",
+        out = "logs/count_kmers.out"
+    shell:
+        """
+        {input.kmer_count_script} -i {input.sras_list} 2> {log.err} 1> {log.out}
+        """
+        
+rule concat_kmer_counts:
+    input:
+        ".kmers_counted",
+        kmer_concat_script = "workflow/scripts/feature_engineering/concat_kmer_counts.py"
+    output:
+        "data/kmer_counts.tsv"
+    log:
+        err = "logs/concat_kmer_counts.err",
+        out = "logs/concat_kmer_counts.out"
+    conda:
+        "data_prep"
+    shell:
+        """
+        {input.kmer_concat_script} 2> {log.err} 1> {log.out}
+        """
---
+++ workflow/scripts/feature_engineering/get_kmer_counts.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+# ---------------------------
+# Prefetch FASTQ files from SRA record numbers using SRA toolkit
+# Author: Paul Villanueva (github.com/pommevilla)
+# ---------------------------
+
+helpFunction()
+{
+   echo ""
+   echo "Usage: $0 -i sras (-n num_records)"
+   echo -e "\t-i List of SRAs to dump, each on a newline"
+   echo -e "\t-n Number of records to fetch (default: 2)"
+   exit 1 
+}
+
+while getopts "i:n:" opt
+do
+   case "$opt" in
+      i ) sra_records="$OPTARG" ;;
+      n ) num_records="$OPTARG" ;;
+      ? ) helpFunction ;; 
+   esac
+done
+
+if [ -z "$sra_records" ] 
+then
+   echo "Some or all of the parameters are empty."
+   helpFunction
+fi
+
+if [ ! -f "$sra_records" ]; then
+    echo "$sra_records does not exist. Are you in the right directory?"
+    helpFunction
+fi
+
+if [ -z "$num_records" ] 
+then
+   num_records=2
+fi
+
+echo "SRA records file: $sra_records"
+echo "Number of records to fetch: $num_records"
+
+echo "Counting k-mers..."
+
+head -n $num_records $sra_records | while read -r sra_record
+do
+   echo "    $sra_record"
+   kmc -k15 data/genomes/${sra_record}/${sra_record}_pass_1.fastq.gz data/kmc_temp/15mers data/kmc_temp
+   kmc_tools transform data/kmc_temp/15mers dump data/kmer_counts/${sra_record}_15mers.txt
+done
